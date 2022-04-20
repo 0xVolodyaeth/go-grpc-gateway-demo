cfd390c26f570115f3d6d76488b2a9f47ea0a842
Vladimir Patsenko <v@patsenko.ru>
2022-04-20 09:40:18 +0300

    init

---
+++ server/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"gogrpcgateway/service_proto"
+	"log"
+	"net"
+
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
+	"google.golang.org/grpc"
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
+)
+
+type ExampleService struct {
+	*service_proto.UnimplementedYourServiceServer
+}
+
+func main() {
+	exs := ExampleService{}
+
+	lis, err := net.Listen("tcp", ":3000")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	srv := grpc.NewServer()
+
+	service_proto.RegisterYourServiceServer(srv, &exs)
+
+	log.Println("GRPC is listening on port 3000")
+	if err = srv.Serve(lis); err != nil {
+		log.Fatalln(err)
+	}
+}
